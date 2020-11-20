dbb297dc4f1376ad0533e8ddcc71db67d0af1186
weslie10 <weslie.leonardo10@gmail.com>
2020-11-20 16:32:01 +0700

    ubah jadi lebih mudah syntaxnya

--- maps.js
+++ maps.js
@@ -5,8 +5,12 @@ select.addEventListener('change',(e)=>{
     e.preventDefault();
     if(e.target.value!==""){
         fetch(`data/${e.target.value}`).then(res=>res.json()).then(result=>{
-            // console.log(result.features);
-            makeMapFeatures(result.features);
+            makeMap();
+            L.geoJSON(result,{style: function(){
+                return {"color": randomColor()}
+            }}).addTo(map);
+            
+            // makeMapFeatures(result.features);
         });
     }
     else{
@@ -36,75 +40,75 @@ const makeMap = () => {
         zoomOffset: -1,
     }).addTo(map);
 }
+makeMap();
 
-const makeMapFeatures = (data) => {
-    makeMap();
+// const makeMapFeatures = (data) => {
+//     makeMap();
 
-    if(data!==""){
-        data.map(el => {
-            const prop = el.properties;
-            let text = "";
-            Object.keys(prop).forEach((key)=> { 
-                text += `${key}: ${prop[key]}<br/>`; // key and value
-            });
+//     if(data!==""){
+//         data.map(el => {
+//             const prop = el.properties;
+//             let text = "";
+//             Object.keys(prop).forEach((key)=> { 
+//                 text += `${key}: ${prop[key]}<br/>`; // key and value
+//             });
 
-            if(el.geometry.type==="MultiPolygon"){
-                const geo = el.geometry.coordinates[0][0]
-                var poly = [];
-                geo.map((element,i) => {
-                    [element[0], element[1]] = [element[1], element[0]];
-                    poly.push(element);
-                });
-                const polygon = L.polygon(poly, { color: randomColor() })
-                    .bindPopup(text)
-                    .addTo(map);
-                polygon.on("mouseover",function(e){
-                    this.openPopup();
-                });
-                polygon.on("mouseout",function(e){
-                    this.closePopup();
-                });
-            }
-            else if(el.geometry.type === "Point"){
-                let coord = el.geometry.coordinates;
-                [coord[0], coord[1]] = [coord[1], coord[0]];
-                const marker = L.marker(coord)
-                    .bindPopup(text)
-                    .addTo(map);
-                marker.on("mouseover",function(e){
-                    this.openPopup();
-                });
-                marker.on("mouseout",function(e){
-                    this.closePopup();
-                });
-            }
-            else if(el.geometry.type === "MultiLineString"){
-                const geo = el.geometry.coordinates[0]
-                var line = [];
-                geo.map((element,i) => {
-                    [element[0], element[1]] = [element[1], element[0]];
-                    line.push(element);
-                });
-                const polyline = L.polyline(line, { color: randomColor() })
-                    .bindPopup(text)
-                    .addTo(map);
-                polyline.on("mouseover",function(e){
-                    this.openPopup();
-                });
-                polyline.on("mouseout",function(e){
-                    this.closePopup();
-                });
-            }
-        });
-    }
+//             if(el.geometry.type==="MultiPolygon"){
+//                 const geo = el.geometry.coordinates[0][0]
+//                 var poly = [];
+//                 geo.map((element,i) => {
+//                     [element[0], element[1]] = [element[1], element[0]];
+//                     poly.push(element);
+//                 });
+//                 const polygon = L.polygon(poly, { color: randomColor() })
+//                     .bindPopup(text)
+//                     .addTo(map);
+//                 polygon.on("mouseover",function(e){
+//                     this.openPopup();
+//                 });
+//                 polygon.on("mouseout",function(e){
+//                     this.closePopup();
+//                 });
+//             }
+//             else if(el.geometry.type === "Point"){
+//                 let coord = el.geometry.coordinates;
+//                 [coord[0], coord[1]] = [coord[1], coord[0]];
+//                 const marker = L.marker(coord)
+//                     .bindPopup(text)
+//                     .addTo(map);
+//                 marker.on("mouseover",function(e){
+//                     this.openPopup();
+//                 });
+//                 marker.on("mouseout",function(e){
+//                     this.closePopup();
+//                 });
+//             }
+//             else if(el.geometry.type === "MultiLineString"){
+//                 const geo = el.geometry.coordinates[0]
+//                 var line = [];
+//                 geo.map((element,i) => {
+//                     [element[0], element[1]] = [element[1], element[0]];
+//                     line.push(element);
+//                 });
+//                 const polyline = L.polyline(line, { color: randomColor() })
+//                     .bindPopup(text)
+//                     .addTo(map);
+//                 polyline.on("mouseover",function(e){
+//                     this.openPopup();
+//                 });
+//                 polyline.on("mouseout",function(e){
+//                     this.closePopup();
+//                 });
+//             }
+//         });
+//     }
 
-    // L.marker([0.570012, 101.425655])
-    //     .addTo(map)
-    //     .bindPopup("This is at 0.570012, 101.425655")
-    //     .openPopup();
+//     // L.marker([0.570012, 101.425655])
+//     //     .addTo(map)
+//     //     .bindPopup("This is at 0.570012, 101.425655")
+//     //     .openPopup();
 
-    // map.addEventListener("click", (e) => {
-    //     console.log(e.latlng);
-    // });
-}
-makeMap("");
+//     // map.addEventListener("click", (e) => {
+//     //     console.log(e.latlng);
+//     // });
+// }
